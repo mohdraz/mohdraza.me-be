02e96c0109e45c38125602f2792c175a7b8115f0
Mohd Raza <noorinfotechinc@gmail.com>
2020-09-04 21:55:38 -0500

    dbconfig

---
+++ database/dbConfig.js
@@ -0,0 +1,7 @@
+import knex from "knex";
+const knex = require("knex");
+
+const knexConfig = require("../knexfile.js");
+
+const environmnet = process.env.DB_ENV || "development";
+module.exports = knex(knexConfig[environmnet]);
